2262d2a2bab3a3f60ab858bc98f39996e9d48927
Maksim Zheleznyakov <saffm1994@gmail.com>
2023-11-09 10:56:57 +0300

    init

--- pkg/repository/metadata/metadata.go
+++ pkg/repository/metadata/metadata.go
@@ -1,24 +1,77 @@
 package metadata
 
 import (
+	"context"
+	"time"
+
 	"github.com/Saffica/image-storage/pkg/models"
+	"github.com/jackc/pgconn"
+	"github.com/jackc/pgx/v4"
 )
 
+type dataBase interface {
+	Close()
+	BeginTx(ctx context.Context) (pgx.Tx, error)
+	Exec(ctx context.Context, query string, arg ...interface{}) (pgconn.CommandTag, error)
+	Query(ctx context.Context, query string, arg ...interface{}) (pgx.Rows, error)
+	QueryRow(ctx context.Context, query string, arg ...interface{}) pgx.Row
+}
+
 type metaDataRepository struct {
+	db dataBase
 }
 
-func New() *metaDataRepository {
-	return &metaDataRepository{}
+func New(db dataBase) *metaDataRepository {
+	return &metaDataRepository{
+		db: db,
+	}
 }
 
-func (r *metaDataRepository) Get(url string) (*models.MetaData, error) {
-	return nil, nil
+func (r *metaDataRepository) Get(ctx context.Context, url string) (*models.MetaData, error) {
+	metadata := &models.MetaData{}
+	row := r.db.QueryRow(
+		ctx,
+		"SELECT metadata_id, download_link, downloaded, updated_at FROM metadata WHERE download_link = $1",
+		url,
+	)
+	err := row.Scan(&metadata.ID, &metadata.DownloadLink, &metadata.Downloaded, &metadata.UpdatedAt)
+	if err != nil {
+		return nil, err
+	}
+
+	return metadata, nil
 }
 
-func (r *metaDataRepository) Insert(metaData *models.MetaData) (*models.MetaData, error) {
-	return nil, nil
+func (r *metaDataRepository) Insert(ctx context.Context, metadata *models.MetaData) (*models.MetaData, error) {
+	row := r.db.QueryRow(
+		ctx,
+		"INSERT INTO metadata (download_link, downloaded) VALUES ($1, $2) RETURNING metadata_id, updated_at",
+		metadata.DownloadLink,
+		metadata.Downloaded,
+	)
+
+	err := row.Scan(&metadata.ID, &metadata.UpdatedAt)
+	if err != nil {
+		return nil, err
+	}
+
+	return metadata, nil
 }
 
-func (r *metaDataRepository) Update(metaData *models.MetaData) (*models.MetaData, error) {
-	return nil, nil
+func (r *metaDataRepository) Update(ctx context.Context, metadata *models.MetaData) (*models.MetaData, error) {
+	now := time.Now()
+	_, err := r.db.Exec(
+		ctx,
+		"UPDATE metadata SET download_link=$1, downloaded=$2, updated_at=$3 WHERE metadata_id=$4",
+		metadata.DownloadLink,
+		metadata.Downloaded,
+		now,
+		metadata.ID,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	metadata.UpdatedAt = now
+	return metadata, nil
 }
